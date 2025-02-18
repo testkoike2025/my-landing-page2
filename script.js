c504efcb5bd68173f696dab7ed48da0997c6257d
testkoike2025 <gwf.koike@gmail.com>
2025-02-18 13:25:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ console.log("JavaScriptファイルが読み込まれました！");
 
 // ここにハンバーガーメニューやスクロールアニメーションのコードを追加
 
+    <script>
+    window.addEventListener("scroll", function() {
+        let elements = document.querySelectorAll(".fade-in");
+        elements.forEach((el) => {
+            let rect = el.getBoundingClientRect();
+            if (rect.top < window.innerHeight - 100) {
+                el.classList.add("visible");
+            }
+        });
+    });
+</script>
